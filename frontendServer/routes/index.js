9def55b455a7cb577cbfff0c4ee3275b72e15946
Alessandro Chetta <alessandrochetta@gmail.com>
2018-05-03 12:08:39 +0200

    adds frontendServer

---
+++ frontendServer/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('*', function(req, res, next) {
+  res.sendfile("public/index.html");
+});
+
+module.exports = router;
